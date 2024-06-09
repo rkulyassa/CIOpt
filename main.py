3b12342d89a16a40a1707c6353447bd5a7246771
ryan <rkulyassa@gmail.com>
2024-06-09 18:57:34 -0400

    evaluate objective fn with energy states

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from reader import Reader
+from writer import Writer
+import numpy as np
+from numpy.typing import NDArray
 
-grad_file = './GRAD/scr.geom/grad.xyz'
-data = Reader(grad_file)
-print(data.grad)
+# User-defined constants:
+ALPHA = 0.02 # Hartree
+SIGMA = 3.5 # Hartree
+STEP_TOL = 0.000001 # Hartree
+GRAD_TOL = 0.0005 # Hartree/Bohr
+
+def step(R: NDArray[np.float64], e_I: float, e_J: float):
+
+    # Calculate average energy of states I and J
+    e_avg = (e_I + e_J) / 2
+
+    # Calculate energy difference of states I and J
+    e_diff = e_I - e_J
+
+    # Evaluate penalty function
+    penalty = (e_diff * e_diff) / (e_diff + ALPHA)
+
+    # Evaluate objective function
+    objective = e_avg + SIGMA * penalty
+
+    # TODO: Determine next state with gradient of objective fn.
+
+if __name__ == '__main__':
+    grad_file = './GRAD/scr.geom/grad.xyz'
+    geom_file = './out.xyz'
+    reader = Reader(grad_file, geom_file)
+    geometry = reader.parse_geom()
+    step(geometry, 5, 6)
--- reader.py
+++ reader.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 class Reader:
-    def __init__(self, grad_file):
-        self.grad = self.parse_grad(grad_file)
+    def __init__(self, grad_file, geom_file):
+        self.grad_file = grad_file
+        self.geom_file = geom_file
+        # self.grad = self.parse_grad(grad_file)
+        # self.geom = self.parse_geom(geom_file)
 
-    def parse_grad(file_path):
-        with open(file_path, 'r') as file:
+    def parse_grad(self, grad_file = None):
+        grad_file = grad_file or self.grad_file
+        with open(grad_file, 'r') as file:
             lines = file.readlines()
         
         grad_data = []
@@ -19,3 +23,6 @@ class Reader:
             grad_data.append([atom, grad_x, grad_y, grad_z])
 
         return np.array(grad_data, dtype=object)
+    
+    def parse_geom(self, geom_file = None):
+        geom_file = geom_file or self.geom_file
