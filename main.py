10e11a30c81bf0e7a9d731058aeb4dd3185a71e0
ryan <rkulyassa@gmail.com>
2024-06-23 20:52:49 -0400

    merge reader and writer to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 # User-defined constants:
 ALPHA = 0.02 # Hartree
@@ -6,7 +7,39 @@ SIGMA = 3.5 # Hartree
 STEP_TOL = 0.000001 # Hartree
 GRAD_TOL = 0.0005 # Hartree/Bohr
 
-def calculate_gradient(e_I: float, e_J: float, d_e: np.ndarray):
+def parse_system_gradient(gradient_file: str) -> np.ndarray:
+    with open(gradient_file, 'r') as file:
+        lines = file.readlines()
+    
+    num_atoms = int(re.sub(r'\D', '', lines[0])) # necessary?
+
+    gradient = []
+    
+    for line in lines[2:]:
+        data = line.split()
+        atom = data[0] # necessary?
+        coordinates = [float(c) for c in data[1:]]
+        gradient.append(coordinates)
+
+    return np.array(gradient, dtype=object)
+
+def parse_geometry(geometry_file: str) -> np.ndarray:
+    with open(geometry_file, 'r') as file:
+        lines = file.readlines()
+    
+    num_atoms = int(re.sub(r'\D', '', lines[0])) # necessary?
+
+    geometry = []
+    
+    for line in lines[2:]:
+        data = line.split()
+        atom = data[0] # necessary?
+        coordinates = [float(re.sub(r'\n', '', c)) for c in data[1:]]
+        geometry.append(coordinates)
+    
+    return np.array(geometry, dtype=object)
+
+def get_objective_gradient(e_I: float, e_J: float, d_e: np.ndarray) -> np.ndarray:
     """
     Based on Levine pg. 407 eq. 7
 
@@ -31,14 +64,28 @@ def calculate_gradient(e_I: float, e_J: float, d_e: np.ndarray):
     # Evaluate objective fn.
     d_obj = d_e_avg + SIGMA * d_pen
 
-    print('Energy difference:', e_diff)
-    print('Energy derivative differences:', d_e_diff)
-    print('Energy derivative averages:', d_e_avg)
-    print('Penalty gradient:', d_pen)
-    print('Objective gradient:', d_obj)
+    # print('Energy difference:', e_diff)
+    # print('Energy derivative differences:', d_e_diff)
+    # print('Energy derivative averages:', d_e_avg)
+    # print('Penalty gradient:', d_pen)
+    # print('Objective gradient:', d_obj)
 
     return d_obj
 
+def steepest_descent(geometry: np.ndarray, gradient: np.ndarray):
+    """
+    Steps a geometric system based on steepest descent gradient method.
+
+    Args:
+
+    """
+
+
+
 if __name__ == '__main__':
-    gradient = calculate_gradient(5, 8, np.array([[1,2,2], [3,1,5]]))
-    print(gradient)
+    # system_gradient = parse_system_gradient('./GRAD/scr.geom/grad.xyz')
+    # print(system_gradient)
+    # objective_gradient = get_objective_gradient(5, 8, np.array([[1,2,2], [3,1,5]]))
+    # print(objective_gradient)
+    geometry = parse_geometry('./GRAD/geom.xyz')
+    print(geometry)
--- writer.py
+++
@@ -1,7 +0,0 @@
-class Writer:
-    def __init__(self, geom_file):
-        self.geom_file = geom_file
-    
-    def write_geom(self, geom_data):
-        with open(self.geom_file, 'w'):
-            pass
